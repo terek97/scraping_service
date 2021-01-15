474c3a9cf3d397e87e3b54b49432308ee395d4a1
terek97 <kurbanmagomedovr@gmail.com>
2021-01-15 17:21:46 +0300

    second commit

--- scraping/admin.py
+++ scraping/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import City
+from .models import City, Vacancy
 from .models import Profession
 
 # Register your models here.
 admin.site.register(City)
 admin.site.register(Profession)
+admin.site.register(Vacancy)
